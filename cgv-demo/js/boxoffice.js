8f3b6a96984b41be07486e0e969e4af808aa7826
tkdudsh <syn_1234@naver.com>
2026-04-06 12:45:46 +0900

    1221

--- cgv-demo/js/boxoffice.js
+++ cgv-demo/js/boxoffice.js
@@ -5,63 +5,84 @@
 const kobis_key=`7e2804fcf19f535083b2211d0ce1be96`
 const kmdb_key = '59H5F0U0OFQB3R2261VM';
 
-async function getKobis(){
-    let url=`http://kobis.or.kr/kobisopenapi/webservice/rest/boxoffice/searchDailyBoxOfficeList.json?`
-    url+=`key=${kobis_key}&targetDt=20120101`
+async function getKobis() {
 
-    let response = await fetch(url)
+    let date=new Date();
+    let year=new String(date.getFullYear())
+    let month=new String(date.getMonth()+1);
+    let day=new String(date.getDate()-1);
+    month=(month.length<2)? '0'.concat(month): month ;
+    day=(day.length<2)? '0'.concat(day): day ;
+    let today= year.concat(month).concat(day) 
+    console.log(today)
+
+    let url = `http://kobis.or.kr/kobisopenapi/webservice/rest/boxoffice`;
+    url += `/searchDailyBoxOfficeList.json?`;
+    url += `key=${kobis_key}`;
+    url += `&targetDt=${today}`;
+
+    let response = await fetch(url);
     return response.json();
 }
 
-/**
- * KMDB API 포스터 가져오기
- */
-async function getPoster(movieNm,openDt){
-
-    openDt = openDt.split('-').reduce((acc,cur)=> acc +cur)
-    console.log(openDt)
-    let url= `http://api.koreafilm.or.kr/openapi-data2/wisenut/search_api`;
+/** KMDB API - 포스터 가져오기 */
+async function getPoster(movieNm, openDt) {
+    openDt = openDt.split('-').reduce((acc, cur) => acc + cur);
+    
+    let url = `http://api.koreafilm.or.kr/openapi-data2/wisenut/search_api`;
     url += `/search_json2.jsp?collection=kmdb_new2&detail=Y`;
     url += `&title=${movieNm}`;
     url += `&releaseDts=${openDt}&ServiceKey=${kmdb_key}`;
 
-    let response=await fetch(url)
-    let kmdb=await response.json()
-    let result=await kmdb?.Data?.[0]?.Result?.[0]?.posters?.split('|');
-    return response.json()
+    let response = await fetch(url);
+    let kmdb = await response.json();
+    let posters = await kmdb?.Data?.[0]?.Result?.[0]?.posters?.split('|');
+    // console.log('result => ' ,posters[0]);
+
+    return posters?.[0];
 }
 
-async function createBoxoffice(){
-    let kobis= await getKobis();
-    let list=kobis.boxOfficeResult.dailyBoxOfficeList;
-    console.log(list)
+async function showList(list) {    
+    let slist = list.slice(0, 5).map(async (movie) => {  
+        let movieNm = movie.movieNm;
+        let openDt = movie.openDt;  
+        let audiAcc = movie.audiAcc;    
+        let poster = await getPoster(movie.movieNm, movie.openDt);           
+        // console.log(movie.movieNm, movie.openDt, poster); 
+        return { movieNm, openDt, audiAcc, poster}            
+    }); 
 
-    let count=0;
+    return await Promise.all(slist);
+}
 
-   
-    let showList=list.slice(0,5).map(async (movie,idx)=>{
-        let movieNm=movie.movieNm;
-        let openDt=movie.openDt;
-        let poster = await getPoster(movie.movieNm,movie.openDt)
-        return {movieNm, openDt, poster}
-
-        `
+async function createBoxoffice() {
+    let kobis = await getKobis();
+    let list = kobis.boxOfficeResult.dailyBoxOfficeList;
+    let outputList = await showList(list);    
+    // console.log('showList => ', outputList); 
+    let output = `
         <ul>
             ${
-                outputList.map((movie)=>{
+                outputList.map((movie) => `
                     <li>
                         <div>
-                            <img src='${movie.poster}'>
-                            </img>
+                            <img src="${movie.poster}" 
+                                alt="moviechart img"
+                                width="200px">
                         </div>
                         <div><h3>${movie.movieNm}</h3></div>
-                        <div><h5>누적 관객수 ${movie.audiAcc}</h5></div>
+                        <div><h5>누적관객수 ${movie.audiAcc}</h5></div>
                     </li>
-                }).join("")
-            }`
-        })
+                `).join("")
+            }
+        </ul>
+    `;  
+
+    // console.log(output);
+    document.querySelector('#boxoffice').innerHTML = output;    
 }
 
-window.addEventListener('DOMContentLoaded',()=>{
-    createBoxoffice()
-})
+
+window.addEventListener('DOMContentLoaded', () => {
+        createBoxoffice();
+});
