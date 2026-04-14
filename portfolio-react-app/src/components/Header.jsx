6bbd8c83c150a74eac312e92ac9aa6150ceb5fb2
tkdudsh <syn_1234@naver.com>
2026-04-14 18:07:59 +0900

    커밋

--- portfolio-react-app/src/components/Header.jsx
+++ portfolio-react-app/src/components/Header.jsx
@@ -1,10 +1,12 @@
 import Logo from "./header/Logo.jsx";
-import MenuList from "./commons/MenuList.jsx";
 import ToggleButton from "./header/ToggleButton.jsx";
+import MenuList from "./commons/MenuList.jsx";
 
-export default function Header({data}) {
-    // const {menus}=data
-    console.log(data)
+export default function Header({ data }) {
+    //✨data 객체가 존재하는 경우에만 구조분해할당 진행! 
+    //✨구조분해 할당으로 반환하는 타입이 객체인 경우에는 초기화 작업 필수
+    const { menus = [] } = data || {};  
+
     return (
         <header class="header">
             <Logo   img="/images/favicon.ico"
@@ -12,8 +14,8 @@ export default function Header({data}) {
                     style="header-logo-img"
                     title="Judy"
             />
-            <MenuList menus={data?.menus} style="header-menu open" />
-            <ToggleButton></ToggleButton>
+            <MenuList menus={menus} style="header-menu open" />
+            <ToggleButton />
         </header>
     )
 }
--- portfolio-react-app/src/util.js/fetch.js
+++ portfolio-react-app/src/util.js/fetch.js
@@ -5,4 +5,5 @@
 export const fetchData=async (url)=>{
 
     const response =await fetch(url);
+    return response.json();
 }
